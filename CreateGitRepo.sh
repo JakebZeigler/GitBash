792b96ea9a59feaedbca6d1485d71cc447abf676
jakebzeigler <27jakebz@gmail.com>
2024-01-03 17:28:10 -0500

    Updated from .txt to .sh

---
+++ CreateGitRepo.sh
@@ -0,0 +1,41 @@
+# HOW TO CREATE AND INIT NEW GIT REPO!
+
+# 1) Open Git Bash and navigate to your project directory:
+cd C:/PATH/
+
+# 2) Then, initialize the repository:
+git init
+
+# 3) Configure your Git username and email (this is a one-time setup):
+git config --global user.name "username"
+git config --global user.email "email@email.com"
+git config --global push.default matching
+git config --global alias.co checkout
+
+# 4) After initializing your repository, add files to it:
+git add .
+
+# 5) Commit the files that you've staged in your local repository:
+git commit -m "Initial commit."
+
+# 6) Create a New Repository on GitHub:
+# Go to GitHub.
+# In the upper-right corner, next to your avatar or identicon, click the '+' sign and then select "New repository."
+# Name your repository, add a description (optional), and choose to make the repository either public or private.
+# Click "Create repository."
+
+# 7) After creating the repository on GitHub, you will see instructions to link your local repository.
+# Replace https://github.com/username/new-repo.git with the URL provided by GitHub.
+git remote add origin https://github.com/username/GitBash.git
+
+# 8) If you haven’t done so already, change the remote URL to include your correct GitHub username:
+git remote set-url origin https://username@github.com/username/repositoryName.git
+
+# 9) Push the changes in your local repository to GitHub:
+git push -u origin master
+
+# 10) If you want to create a new branch and switch to it, use:
+git checkout -b new-branch-name
+
+# 11) If you want to update the master branch, use:
+git add . && git commit -m "Update" && git push origin master
---
+++ GenerateSSHKey.sh
@@ -0,0 +1,26 @@
+# HOW TO GENERATE AN SSH KEY FOR GIT!
+# 1) Open Git Bash and enter the following command, replacing your_email@example.com with your email:
+# When prompted to "Enter a file in which to save the key," press Enter to accept the default file location.
+ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+
+# 2) Add Your SSH Key to the ssh-agent:
+eval "$(ssh-agent -s)"
+
+# 3) Then, add your SSH private key to the ssh-agent:
+ssh-add ~/.ssh/id_rsa
+
+# 4) Add Your SSH Public Key to Your GitHub Account:
+# Run cat ~/.ssh/id_rsa.pub to display your SSH public key. Copy the SSH public key to your clipboard.
+cat ~/.ssh/id_rsa.pub
+
+# 5) On GitHub, go to "Settings" > "SSH and GPG keys."
+# Click on "New SSH key," paste your SSH key, and save it.
+
+# 6) Change the remote URL of your repository from HTTPS to SSH.
+# Replace your-repository-url with your actual repository SSH:
+git remote set-url origin git@github.com:username/repositorySSHTok.git
+
+# 7) Now, you can push changes using SSH without being prompted to log in via a browser:
+git add .
+git commit -m "Update"
+git push origin master
